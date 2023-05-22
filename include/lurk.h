ac6fb35c34c7ae8912ae7dba5d8df0fc45cb5129
Casey Walker <kcmw3e@gmail.com>
2023-05-22 02:44:55 -0400

    more reformatting of macros, using new cat macro
    added LURK_STR_SPACECAT to streamline putting spaces between strings in
    macros

--- include/lurk.h
+++ include/lurk.h
@@ -17,6 +17,7 @@
 #define LURK_STRINGIZE(x) #x
 #define LURK_DEFLECT_STRINGIZE(x) LURK_STRINGIZE(x)
 #define LURK_LINE_STRING LURK_DEFLECT_STRINGIZE(__LINE__)
+#define LURK_STR_SPACECAT(str0, str1) str0" "str1
 
 #include "result.h"
 
